b43dfaf89796c8ceef727111ece088ada536f519
gugamm <gugamm@live.com>
2015-11-17 00:50:01 -0200

    Add support to get surgeries from a certain hospital

--- app/Models/Hospital.php
+++ app/Models/Hospital.php
@@ -11,15 +11,23 @@ class Hospital {
   }
 
   public static function getSurgeries($id) {
-      /* SELECT s.name, s.specialty, s.crm FROM hospital AS h
-        INNER JOIN hospital_surgeryform AS hsf
-        	ON h.id = hsf.idHospital
-        INNER JOIN surgeryform AS sf
-        	ON hsf.idsurgeryform = sf.id
-        INNER JOIN surgeon_surgeryform AS ssf
-        	ON sf.id = ssf.idsurgeryform
-        INNER JOIN surgeon AS s
-        	ON ssf.idsurgeon = s.email; */
+      $result = Database::queryParams('SELECT s.name, s.specialty, s.crm FROM hospital AS h
+                                       INNER JOIN hospital_surgeryform AS hsf
+                                       ON h.id = hsf.idHospital
+                                       INNER JOIN surgeryform AS sf
+                                       ON hsf.idsurgeryform = sf.id
+                                       INNER JOIN surgeon_surgeryform AS ssf
+                                       ON sf.id = ssf.idsurgeryform
+                                       INNER JOIN surgeon AS s
+                                       ON ssf.idsurgeon = s.email
+                                       WHERE h.id = $1',Array($id));
+      $surgeries = array();
+      while ($row = pg_fetch_row($result))
+          $surgeries[] = array("surgeonName"      => $row[0],
+                               "surgeonSpecialty" => $row[1],
+                               "surgeonCrm"       => $row[2]);
+
+      return $surgeries;
   }
 }
 
